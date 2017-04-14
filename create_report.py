097af471ecdfb80454cfcbb9a8d136983d62fd72
Jon Wheeler <jonathanwheeler1@weber.edu>
2017-04-14 12:58:47 -0600

    Added files

---
+++ create_report.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main():
+    """
+    test function
+    """
+    pass
+
+
+if __name__ == "__main__"":
+    main()
+
+    exit(0)
+
---
+++ run_report.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: run_report.sh
+#
+#         USAGE: ./run_report.sh
+#
+#   DESCRIPTION:
+#
+#        AUTHOR: Dr. Hugo Valle (), hugovalle1@weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 04/14/2017 12:57:54 PM
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+exit(0)
+
