319bcd8c7ba8d8faed8131c695022ba29a49d128
Trevor Orgill <trevororgill@weber.edu>
2017-04-14 19:37:48 -0600

    Mostly working .py Script

--- create_report.py
+++ create_report.py
@@ -1,16 +1,55 @@
 #!/usr/bin/env python3
 import sys
+#CS3030 Assignment
+#Author: Trevor Orgill
+#Description: Takes in a Date and formats it to date time
 
 
-def main():
+def formatDate(date, time):
     """
-    test function
+    Takes in date (YYYYMMDD) and formats it to Date-Time (YYYY-MM-DD hh:mm)
+    Pass in the date and the time (beg or end)
     """
-    pass
+    if (len(date) != 8):
+        print("Invalid date length")
+        exit(1)
+    if (time == "beg"):
+        return (date[:4]+"-"+date[4:6]+"-"+date[6:8]+" 00:00")
+    elif (time == "end"):
+        return (date[:4]+"-"+date[4:6]+"-"+date[6:8]+" 23:59")
+    else:
+        print("Invalid Time Parameter (beg or end)")
+        exit(1)
 
 
-if __name__ == "__main__"":
-    main()
+def usage():
+    """
+    Usage Function
+    """
+    print("Takes in 2 dates (YYYYMMDD) and displays them in Date-Time Format (YYYY-MM-DD hh:mm)")
+    exit(1)
+
+
+def main(argv):
+    """
+    Main Function
+    """
+    if (argv[1] == "--help"):
+        usage()
+    if (len(argv) != 3):
+        print("Invalid number of arguments")
+        usage()
+    if (len(argv[1]) != 8):
+        print("Invalid date length")
+        usage()
+    if (len(argv[2]) != 8):
+        print("Invalid date length")
+        usage()
+    print(formatDate(argv[1], "beg"))
+    print(formatDate(argv[2], "end"))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
 
     exit(0)
-
